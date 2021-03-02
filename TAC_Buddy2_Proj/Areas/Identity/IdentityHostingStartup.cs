273a233b5be80c3361e52fb9ceea0ce4fbd53d8d
kyledobash <kyledobash@yahoo.com>
2021-03-02 09:14:20 -0700

    init, with Identity configured

---
+++ TAC_Buddy2_Proj/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TAC_Buddy2_Proj.Data;
+
+[assembly: HostingStartup(typeof(TAC_Buddy2_Proj.Areas.Identity.IdentityHostingStartup))]
+namespace TAC_Buddy2_Proj.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ TAC_Buddy2_Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TAC_Buddy2_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
