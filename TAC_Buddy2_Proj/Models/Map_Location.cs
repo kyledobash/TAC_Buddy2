b5eb57a3fccec0ab66310c466fc5b9756ed224a0
kyledobash <kyledobash@yahoo.com>
2021-03-02 13:08:17 -0700

    Added Map Location model

---
+++ TAC_Buddy2_Proj/Models/Map_Location.cs
@@ -0,0 +1,26 @@
+﻿using CoordinateSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TAC_Buddy2_Proj.Models
+{
+    public class Map_Location
+    {
+        [Key]
+        public double Location_ID { get; set; }
+
+        [ForeignKey("TAC_TeamLeader_ID")]
+        public double TAC_TeamLeader_ID { get; set; }
+        public TAC_TeamLeader TAC_TeamLeader { get; set; }
+
+        public MilitaryGridReferenceSystem MGRS_Coords { get; set; }
+
+        public double Elevation { get; set; }
+
+        public Coordinate Lat_Long { get; set; }
+    }
+}
